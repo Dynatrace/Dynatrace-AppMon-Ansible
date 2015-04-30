ded72543d936e531bcce144a17260299a0e28073
Martin Etmajer <m.etmajer@gmail.com>
2015-04-30 12:41:15 +0200

    Remove unnecessary should_be_file check.

--- test/integration/tomcat-agent/serverspec/tomcat-agent_spec.rb
+++ test/integration/tomcat-agent/serverspec/tomcat-agent_spec.rb
@@ -4,6 +4,5 @@ require 'serverspec'
 set :backend, :exec
 
 describe file('/tmp/environment') do
-  it { should be_file }
   its(:content) { should match /^export CATALINA_OPTS="\$CATALINA_OPTS -agentpath:\/opt\/dynatrace\/agent\/lib64\/libdtagent.so=name=apache-tomcat-agent,collector=localhost:9998"$/ }
 end
