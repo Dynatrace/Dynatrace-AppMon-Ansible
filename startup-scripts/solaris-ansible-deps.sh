e3e581ede208153067e73fd77ed55cdfb0d9d702
Anthony Kalinde <anthony.kalinde@gmail.com>
2018-09-13 19:43:04 +0200

    check downloaded installer previously or for installed version

--- startup-scripts/solaris-ansible-deps.sh
+++ startup-scripts/solaris-ansible-deps.sh
@@ -14,19 +14,19 @@ echo "Installing stuff ---->"
 /opt/csw/bin/pkgutil -y -i python
 /opt/csw/bin/pkgutil -y -i ansible
 /opt/csw/bin/pkgutil -y -i vim
-/opt/csw/bin/pkgutil -y -i jdk8
+# /opt/csw/bin/pkgutil -y -i jdk8
+#
+# # set java home
+#
+# cat <<EOF >> $ENV_FILE
+#   export JAVA_HOME=/opt/csw/java/jdk/jdk8/
+#   export PATH=$PATH:$JAVA_HOME:bin:$JAVA_HOME/jre/bin
+# EOF
+#
+# cat <<EOF >> $USER_ENV_FILE
+#   export JAVA_HOME=/opt/csw/java/jdk/jdk8/
+#   export PATH=$PATH:$JAVA_HOME:bin:$JAVA_HOME/jre/bin
+# EOF
 
-# set java home
-
-cat <<EOF >> $ENV_FILE
-  export JAVA_HOME=/opt/csw/java/jdk/jdk8/
-  export PATH=$PATH:$JAVA_HOME:bin:$JAVA_HOME/jre/bin
-EOF
-
-cat <<EOF >> $USER_ENV_FILE
-  export JAVA_HOME=/opt/csw/java/jdk/jdk8/
-  export PATH=$PATH:$JAVA_HOME:bin:$JAVA_HOME/jre/bin
-EOF
-
-source $ENV_FILE
-java -version
+# source $ENV_FILE
+# java -version
